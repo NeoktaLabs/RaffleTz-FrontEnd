56193a99415a7f4695d5d7fe1e1418ff4e0b0b90
garinlu <lucas.garin73@gmail.com>
2022-08-21 11:35:47 +0200

    Fix conflicts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,9 @@ import Layout from "../components/layout/Layout";
 import HomeContainer from "../components/home/HomeContainer";
 
 const IndexPage = () => (
-<<<<<<< HEAD
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js Dev 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-=======
     <Layout title="Home | RaffleTz">
         <HomeContainer/>
     </Layout>
->>>>>>> init-project
 )
 
 export default IndexPage
